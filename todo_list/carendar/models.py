0f96071a339f3df6dfd7d4909ce8cca09eb0e57e
kaito furuya <kaito.furuya0806@gmail.com>
2024-09-20 03:36:01 +0900

    update carendar/models

--- todo_list/carendar/models.py
+++ todo_list/carendar/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class Todo(models.Model):
+    dt = models.DateTimeField(verbose_name="投稿日時", default=timezone.now)
+    deadline = models.DateTimeField(verbose_name="期日")  # 変更: DateField -> DateTimeField
+    content = models.CharField(verbose_name="やること", max_length=300)
+    done = models.BooleanField(verbose_name="やった", default=False)
+
+    def __str__(self):
+        return self.content
+
