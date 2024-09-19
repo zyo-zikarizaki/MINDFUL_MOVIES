944d7f2564c9ba7f14ee153dd0aefa1b4c3eddcd
kaito furuya <kaito.furuya0806@gmail.com>
2024-09-20 04:43:50 +0900

    update todo_list/urls.py and carendar/views.py

--- todo_list/todo_list/urls.py
+++ todo_list/todo_list/urls.py
@@ -1,10 +1,16 @@
 
 from django.contrib import admin
 from django.urls import path,include
+from django.contrib.auth import views as auth_views
+from carendar import views
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("",include("carendar.urls")),
     path('movies/', include('movies.urls')),
+    path('accounts/login/',auth_views.LoginView.as_view(), name='login'),
+    path('accounts/logout/',views.logout_view, name='logout'),
+    path('accounts/signup/',views.signup, name='signup'),
+    path('accounts/profile/',views.profile, name='profile'),
 ]
